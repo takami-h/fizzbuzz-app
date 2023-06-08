6b67a0dafae82eadef0990f6a7f477462fab55b0
Takami Hirata <26153825+takami-h@users.noreply.github.com>
2023-06-08 11:51:23 +0900

    feat: cors

--- fizzbuzz-api/src/main/java/com/redhat/labs/fizzbuzz/adapter/FizzBuzzController.java
+++ fizzbuzz-api/src/main/java/com/redhat/labs/fizzbuzz/adapter/FizzBuzzController.java
@@ -1,12 +1,14 @@
 package com.redhat.labs.fizzbuzz.adapter;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.redhat.labs.fizzbuzz.application.FizzBuzz;
 
+@CrossOrigin
 @RestController
 public class FizzBuzzController {
   private FizzBuzz fizzbuzz;
